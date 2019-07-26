26b79cd3c4289cc803deb75b8a6e0091617946ba
srujan-kothapally <mssrujan12564@gmail.com>
2019-07-26 15:57:10 -0400

    initial

---
+++ src/userprog/syscall.c
@@ -0,0 +1,48 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+#include "process.h"
+
+static void syscall_handler (struct intr_frame *);
+
+extern bool running;
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void
+syscall_handler (struct intr_frame *f UNUSED) 
+{
+  int * p = f->esp;
+
+
+  int system_call = * p;
+    
+	switch (system_call)
+	{
+		case SYS_HALT:
+		shutdown_power_off();
+		break;
+
+		case SYS_EXIT:
+		thread_current()->parent->ex = true;
+		thread_exit();
+		break;
+
+		case SYS_WRITE:
+		printf("%s","entryto syscall");
+		if(*(p+5)==1)
+		{
+			putbuf(*(p+6),*(p+7));
+		}
+		break;
+
+		default:
+		printf("No match\n");
+	}
+}
